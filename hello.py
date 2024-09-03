ee46ffe868a12097aad306f827952d637a02221d
Your Name <you@example.com>
2024-09-04 00:21:43 +0800

    Add initial version of hello script

---
+++ hello.py
@@ -0,0 +1,15 @@
+# hello.py
+def CICD_project():
+    print("------Hello, world!------")
+    print(">>Instruction completed<<")
+    print(".------------------------")
+    print("|  .---------------------")
+    print("|  |  .------------------")
+    print("|  |  |  .---------------")
+    print("|  |  |  |               ")
+    print("--------Building!--------")
+    print("-------------------------")
+
+
+if __name__ == "__main__":
+    CICD_project()
